a3680975241bb1cbb4b52e1ceb1783c0f837f6ba
Kunal Singh <16257541+velociraptor98@users.noreply.github.com>
2024-02-25 15:45:22 +0000

    Updating fixed nav component

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,9 @@ import * as React from "react"
 import {
   ChakraProvider,
   Box,
-  Text,
-  Link,
   VStack,
-  Code,
-  Grid,
-  theme, Heading, HStack, Container,
+  theme, Container,
 } from "@chakra-ui/react"
-import { ColorModeSwitcher } from "./ColorModeSwitcher"
 import Navbar from "./Components/Navbar";
 
 export const App = () => (
@@ -17,12 +12,7 @@ export const App = () => (
     <Navbar/>
     <Box textAlign="center" fontSize="xl">
       <Container minH="100vh" p={3}>
-        <HStack><ColorModeSwitcher justifySelf="flex-end" />
-          <Heading>Sponsor List Viewer</Heading>
-        </HStack>
-
         <VStack spacing={8}>
-
         </VStack>
       </Container>
     </Box>
